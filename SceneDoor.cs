011c2bcf5f061fc88bee028099cf71747b6eff95
Jelmer Snippe <jelmer.snippe@vinci-energies.net>
2024-08-09 17:49:44 +0200

    Add direction check to DoorScene

--- SceneDoor.cs
+++ SceneDoor.cs
@@ -5,13 +5,19 @@ public partial class SceneDoor : Area2D {
 	[Export] public string TargetSceneName;
 	[Export] public string TargetDoorId;
 	[Export] public string Id;
-	[Export] public Vector2 Direction;
+	[Export] public Vector2I Direction;
 
 	private bool _disabled = false;
 
 	public override void _Ready() {
 		BodyEntered += OnBodyEntered;
 		BodyExited += OnBodyExited;
+
+		Direction = new Vector2I(Mathf.Sign(Direction.X), Mathf.Sign(Direction.Y));
+
+		if (Direction.X != 0 && Direction.Y != 0) {
+			throw new Exception($"Direction for SceneDoor {Id} is not one of the cardinal directions");
+		}
 	}
 
 	private void OnBodyExited(Node2D body) {
@@ -24,6 +30,14 @@ public partial class SceneDoor : Area2D {
 
 	private void OnBodyEntered(Node2D body) {
 		if (_disabled || body is not Player) {
+			return;
+		}
+
+		Vector2 directionToBody = GlobalPosition.DirectionTo(body.GlobalPosition);
+		int xDirection = Mathf.Sign(directionToBody.X);
+		int yDirection = Mathf.Sign(directionToBody.Y);
+		if ((Direction.X != 0 && xDirection != Direction.X) ||
+			(Direction.Y != 0 && yDirection != Direction.Y)) {
 			return;
 		}
 
