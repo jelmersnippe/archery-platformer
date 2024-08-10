cd63cbe05518515dd29d8b3578fb0c97cf6c359b
Jelmer Snippe <jelmersnippe@hotmail.com>
2024-08-10 13:32:36 +0200

    Add dynamic draw time to bow determining arrow velocity

--- Bow.cs
+++ Bow.cs
@@ -3,7 +3,22 @@ using Godot;
 public partial class Bow : Node2D {
 	[Export] public AnimationPlayer BowAnimationPlayer = null!;
 	[Export] public Node2D FiringPoint = null!;
+	[Export] public float MaxDrawTime = 1f;
+	[Export] public float MinArrowVelocity = 400f;
+	[Export] public float MaxArrowVelocity = 1000f;
+
+	private float _drawTime;
+
 	private Arrow? _currentArrow;
+
+	public override void _PhysicsProcess(double delta) {
+		if (_currentArrow == null) {
+			_drawTime = 0f;
+			return;
+		}
+
+		_drawTime = Mathf.Min(_drawTime + (float)delta, MaxDrawTime);
+	}
 
 	public void ReadyArrow(Arrow arrow) {
 		arrow.Position = Vector2.Zero;
@@ -23,7 +38,8 @@ public partial class Bow : Node2D {
 			return;
 		}
 
-		Vector2 velocity = GlobalPosition.DirectionTo(GetGlobalMousePosition()) * 1000f;
+		Vector2 velocity = GlobalPosition.DirectionTo(GetGlobalMousePosition()) *
+						   Mathf.Lerp(MinArrowVelocity, MaxArrowVelocity, _drawTime / MaxDrawTime);
 		_currentArrow.Release(velocity);
 
 		_currentArrow = null;
