6dfc6bae840be0b578c4df80bcf09163730038fd
Jelmer Snippe <jelmer.snippe@vinci-energies.net>
2024-08-15 12:49:10 +0200

    Add camera lookahead, smoothing and lookahead deadzone

--- Camera.cs
+++ Camera.cs
@@ -2,7 +2,14 @@ using Godot;
 
 public partial class Camera : Camera2D {
 	private Player? _target;
-	[Export] public Area2D Area2D;
+	[Export] public Area2D Area2D = null!;
+	[Export] public float SmoothingX = 0.06f;
+	[Export] public float SmoothingY = 0.02f;
+	[Export] public float LookAheadX = 48f;
+	[Export] public float FallingLookAheadY = 96f;
+	[Export] public float DirectionFlipDeadZone = 128f;
+
+	private int _lookAheadDirection;
 
 	public override void _EnterTree() {
 		SceneTransitionHandler.Instance.PlayerSpawned += InstanceOnPlayerSpawned;
@@ -46,6 +53,18 @@ public partial class Camera : Camera2D {
 			return;
 		}
 
-		GlobalPosition = _target.GlobalPosition;
+		var targetDirection = Mathf.Sign(_target.Velocity.X);
+		if (targetDirection != 0 && targetDirection != _lookAheadDirection) {
+			if (GlobalPosition.DistanceTo(_target.GlobalPosition) < DirectionFlipDeadZone) {
+				return;
+			}
+			_lookAheadDirection = targetDirection;
+		}
+
+		var targetPosition = _target.GlobalPosition +  new Vector2(_lookAheadDirection * LookAheadX, _target.Velocity.Y > 0 ? FallingLookAheadY : 0f);
+		GlobalPosition = new Vector2() {
+			X = Mathf.Lerp(GlobalPosition.X, targetPosition.X, SmoothingX),
+			Y = Mathf.Lerp(GlobalPosition.Y, targetPosition.Y, SmoothingY),
+		};
 	}
 }
